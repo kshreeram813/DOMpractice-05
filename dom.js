7de3c221705530b2d0ca8d3094b1e290d403e962
SHREERAM KUMAR KUSHWAHA <117209741+kshreeram813@users.noreply.github.com>
2023-04-03 17:02:25 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,50 +1,56 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-// var filter = document.getElementById('filter');
+var form11 = document.getElementById('addForm');            // submit and text khatir leleba    // add karta topup per
+var itemList77 = document.getElementById('items11');        /// inside item "li" khatir
+
 
 // Form submit event
-form.addEventListener('submit', addItem);
+form11.addEventListener('submit', addItem77);   // submit & click standar value haa // submit matlab jab hamni submit pe click karjai tab submit hojai
+
+
 // Delete event
-itemList.addEventListener('click', removeItem);
-// // Filter event
-// filter.addEventListener('keyup', filterItems);
+itemList77.addEventListener('click', removeItem33);   // click bi standard haa // jab click karjai tab delete hojai
+
 
 // Add item
-function addItem(e){
-  e.preventDefault();
+function addItem77(evv){
+  evv.preventDefault();
+
 
   // Get input value
-  var newItem = document.getElementById('item').value;
+  var newItem55 = document.getElementById('shree').value;   // extra text add kare khari use hota 
+
 
   // Create new li element
-  var li = document.createElement('li');
+  var li22 = document.createElement('li');      // extra text add karta "li" me
+
   // Add class
-  li.className = 'list-group-item';
+  li22.className = 'list-group-item';  // lining milawta box ke under likhta  // list-group-item -- box me likhta
+
   // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+  li22.appendChild(document.createTextNode(newItem55));      // naya text add karta
 
   // Create del button element
-  var deleteBtn = document.createElement('button');
+  var deleteBtn99 = document.createElement('buttonss');      //creating delet button // we can put anyting in place of "button"
 
   // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  deleteBtn99.className = 'btn btn-danger btn-sm float-right delete11';      // distance me red color ke banawta   extra "Z"
 
   // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
+  deleteBtn99.appendChild(document.createTextNode('Z'));       // append karawta extra "Z" Sign  "li" me jawan hamni likhtanisan box me
 
   // Append button to li
-  li.appendChild(deleteBtn);
+  li22.appendChild(deleteBtn99);     // "li me append karta "Z"  
 
   // Append li to list
-  itemList.appendChild(li);
+  itemList77.appendChild(li22);        //  append karawta "z"   // 
 }
 
 // Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
+function removeItem33(ess){
+  if(ess.target.classList.contains('delete11')){
+   if(confirm('Are You Sure?'))    // puchta ki are you sure to delete // agar comment kardetani ta naa puchi direct delet hojai 
+    {
+      var ravi = ess.target.parentElement;
+      itemList77.removeChild(ravi);
     }
   }
 }
